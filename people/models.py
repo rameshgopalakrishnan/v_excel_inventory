267eadef5b9e3e8fff0f2b709325ca9674943365
KunalB <kunal.bhagawati@yahoo.com>
2016-11-12 19:18:05 +0530

    Add new migrations

--- people/models.py
+++ people/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Customer(models.Model):
     name = models.TextField()
     address = models.TextField()
-    phone_number = models.IntegerField(max_length=10)
+    phone_number = models.IntegerField()
     email_id = models.EmailField()
