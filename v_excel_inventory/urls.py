d382eb16aeb8f48c288e41040d237f0278a205ed
KunalB <kunal.bhagawati@yahoo.com>
2016-11-12 19:40:15 +0530

    Put auth views

--- v_excel_inventory/urls.py
+++ v_excel_inventory/urls.py
@@ -30,5 +30,6 @@ api_urls = [
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(router.urls))
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
