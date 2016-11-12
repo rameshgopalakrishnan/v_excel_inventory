409d99b7595c6faa206f59ac27cd507f5551a49a
KunalB <kunal.bhagawati@yahoo.com>
2016-11-12 19:39:29 +0530

    Add rest framework to installed apps

--- service_and_process/admin.py
+++ service_and_process/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from service_and_process.models import MasterWorkable, MasterAttribute
 
-# Register your models here.
+
+@admin.register(MasterAttribute)
+class MasterAttributeAdmin(admin.ModelAdmin): pass
+
+
+@admin.register(MasterWorkable)
+class MasterWorkableAdmin(admin.ModelAdmin): pass
