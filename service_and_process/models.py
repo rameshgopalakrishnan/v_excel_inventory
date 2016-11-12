3be6a89e0a7efd41e4b8e3eed1635b15e8e36684
KunalB <kunal.bhagawati@yahoo.com>
2016-11-12 23:53:31 +0530

    Put service/product mapping to process

--- service_and_process/models.py
+++ service_and_process/models.py
@@ -34,15 +34,26 @@ class MasterService(models.Model):
     tax = models.DecimalField(max_digits=5, decimal_places=2)
 
 
-class MasterProcess(models.Model):
-    """e.g. Billing, Sorting, QC, Washing"""
-
-    label = models.TextField()
-    service = models.ManyToManyField(MasterService)
-    user = models.ManyToManyField(User, through='MappingUserProcess')
-
-
 class MasterProduct(models.Model):
     """Cookies"""
 
     label = models.TextField()
+    shelf_life_in_days = models.IntegerField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    tax = models.DecimalField(max_digits=4, decimal_places=2)
+
+
+class MasterProcess(models.Model):
+    """e.g. Billing, Sorting, QC, Washing"""
+
+    label = models.TextField()
+    category = models.ForeignKey('people.MasterUnit')
+
+
+class MappingProductServicesProcess(models.Model):
+
+    """e.g., Cookies -- bake, Knead, etc """
+
+    process = models.ForeignKey(MasterProcess)
+    service = models.ForeignKey(MasterService)
+    product = models.ForeignKey(MasterProduct)
