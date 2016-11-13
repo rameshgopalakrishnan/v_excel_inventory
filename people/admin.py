51afbf46215696c1812cc15ed294d7535d0a053c
janani14iyer@gmail.com <janani14iyer@gmail.com>
2016-11-13 05:58:47 +0530

    internal user

--- people/admin.py
+++ people/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import MasterSection
+from .models import MasterSection, InternalUser
 
 
 @admin.register(MasterSection)
 class MasterSectionAdmin(admin.ModelAdmin): pass
+
+@admin.register(InternalUser)
+class MasterInternalUserAdmin(admin.ModelAdmin): pass
